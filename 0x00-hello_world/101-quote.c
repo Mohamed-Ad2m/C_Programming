1d89d2f2225aa85d2fe183fbbb1367f1565fecb8
Adam <mohammedaddame@gmail.com>
2024-08-20 13:09:15 +0200

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A c program that prints to the standard error using write
+ *
+ * Return: 0 (Success)
+ */
+
+int     main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (0);
+}
