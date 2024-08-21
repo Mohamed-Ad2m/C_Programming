7be41522e6e3e752a08ed2e2cbb89d49d391d1f4
Adam <mohammedaddame@gmail.com>
2024-08-21 14:51:23 +0200

    3 comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+int	main(void)
+{
+	int		n1;
+	int		n2;
+
+	n1 = 0;
+	while (n1 < 9)
+	{
+		n2 = n1 + 1;
+		while (n2 <= 9)
+		{
+			putchar(n1 + 48);
+			putchar(n2 + 48);
+			if (n1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return (0);
+}
