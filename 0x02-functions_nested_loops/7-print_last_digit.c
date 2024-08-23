70f251581964fc8df6829a421dc16fb8b262f96b
Adam <mohammedaddame@gmail.com>
2024-08-23 11:50:39 +0200

    fix the INT_MIN with casting && ternaries

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number.
+ * print_last_digit - Prints the last digit of a number
  *
- * @n: The number in question.
+ * @n: The number
  *
- * Return: Value of the last digit.
+ * Return: Value of the last digit
  */
 int print_last_digit(int n)
 {
