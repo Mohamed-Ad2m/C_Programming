753cc7df8c053e1711d852c13329adfd5e0fb564
Adam <mohammedaddame@gmail.com>
2024-08-26 15:34:58 +0200

    write

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Print from 0 to 9 except 2 & 4
+ *						followed b \n
+ */
+void print_most_numbers(void)
+{
+	char	n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		if (n != '2' && n != '4')
+			write(1, &n, 1);
+		n++;
+	}
+	write(1, "\n", 1);
+}
