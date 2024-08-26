ad6ffc4762f79e000e0af6df8e626e3b0b0e3c37
Adam <mohammedaddame@gmail.com>
2024-08-26 15:32:21 +0200

    write

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_numbers - Print from 0 to 9, followed b \n
+ */
+void print_numbers(void)
+{
+	char	n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		write(1, &n, 1);
+		n++;
+	}
+	write(1, "\n", 1);
+}
