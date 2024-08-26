36e130a5904f775f44d13ad7a72c2704669bb8a9
Adam <mohammedaddame@gmail.com>
2024-08-26 15:20:11 +0200

    man isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The character
+ *
+ * Return: 1 if @c is upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
