0f26973ef984c72b14c26ef7038da55d49febe8b
Adam <mohammedaddame@gmail.com>
2024-08-26 16:02:16 +0200

    _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,9 @@ void more_numbers(void)
 		n = 0;
 		while (n <= 14)
 		{
-			if (n <= 9)
-				_putchar(n + 48);
-			else if (n > 9)
-			{
+			if (n > 9)
 				_putchar(n / 10 + 48);
-				_putchar(n % 10 + 48);
-			}
+			_putchar(n % 10 + 48);
 			n++;
 		}
 		_putchar('\n');
