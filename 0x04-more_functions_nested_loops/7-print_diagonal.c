8d383a1788574a292a53062030955251bfa70516
Adam <mohammedaddame@gmail.com>
2024-08-29 21:16:27 +0200

    _putchar

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a straight line in the terminal
+ * @n: The number of times the character \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
