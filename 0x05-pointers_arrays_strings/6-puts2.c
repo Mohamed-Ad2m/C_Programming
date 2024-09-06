c5be1d6dab1ed41446250e0b48a99e428bf38bda
Adam <mohammedaddame@gmail.com>
2024-09-06 11:59:17 +0200

    print even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * puts2 - prints every other character of a string,
+ *		   starting with the first character
+ * @str: The string
+ */
+void puts2(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			write(1, &str[i], 1);
+		i++;
+	}
+}
