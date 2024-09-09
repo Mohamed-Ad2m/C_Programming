038bf4c0f26beacb0ad85656089c9678fd73ce6e
Adam <mohammedaddame@gmail.com>
2024-09-09 13:13:11 +0200

    man

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,16 +13,14 @@ int _atoi(char *s)
 	i = 0;
 	sign = 1;
 	r = 0;
-	while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
-		i++;
-	while (s[i] == '-' || s[i] == '+')
+	if (!*s)
+		return (0);
+	while (!(s[i] >= '0' && s[i] <= '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	while (!(s[i] >= '0' && s[i] <= '9'))
-		i++;
 	while (s[i] >= '0' && s[i] <= '9')
 		r = (r * 10) + (s[i++] - '0');
 	return (sign * r);
