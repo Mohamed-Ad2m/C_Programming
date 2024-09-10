cc221f6caa1983325faece3b4d8f039bc8f9f904
Adam <mohammedaddame@gmail.com>
2024-09-10 20:40:54 +0200

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,20 +7,22 @@
 char *leet(char *s)
 {
 	int		i;
+	char	*leet = "aAeEoOtTlL";
+	char	*t = "4433007711";
 
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		while (*leet)
+		{
+			if (s[i] == *leet)
+			{
+				s[i] = *t;
+				break;
+			}
+			leet++;
+			t++;
+		}
 		i++;
 	}
 	return (s);
