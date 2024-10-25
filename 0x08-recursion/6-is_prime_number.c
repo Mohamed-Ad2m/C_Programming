a1ad93317ecb9cc0063cb6d13ef79e7c475111d8
Adam <mohammedaddame@gmail.com>
2024-10-25 17:37:35 +0200

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,41 @@
 /**
- * is_prime_number - Check whether a num is prime number or not
- * @n: Integer number
+ * _sqrt_recursion - Calculate the square root of @n
+ * @n: The number
  *
- * Return: 1 if the input integer is a prime number,
- *		   otherwise 0.
+ * Return: The square root of @n, -1 otherwise
  */
-int is_prime_number(int n)
+int _sqrt(int n)
 {
 	int		i;
 
-	if (n < 2)
-		return (0);
+	i = 1;
+	while (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * is_prime - Check whether @n is a prime number
+ * @n: The number to be checked
+ *
+ * Return: 1 if it's, 0 otherwise
+ */
+int		is_prime(int n)
+{
+	int		i;
+	int		stp;
 
 	i = 2;
-	while (i < n)
+	stp = sqrt(n);
+	while (i <= stp)
 	{
 		if (n % i == 0)
 			return (0);
 		i++;
 	}
-	return (1);
+	return (n < 2 ? 0 : 1);
 }
